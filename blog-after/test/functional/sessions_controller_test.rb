b0f64f28454176c6b9942531ffb4aea4beb57bfa
Nur Sah Ketene <nursahketene@gmail.com>
2013-03-14 01:55:11 +0200

    Added login method and test for destroying test

--- blog-after/test/functional/sessions_controller_test.rb
+++ blog-after/test/functional/sessions_controller_test.rb
@@ -3,10 +3,15 @@ require 'test_helper'
 class SessionsControllerTest < ActionController::TestCase
   def setup
     @user = User.new( email: "example@email.com",
-                      password: "1234",
-                      password_confirmation: "1234")
+                     password: "1234",
+                     password_confirmation: "1234")
     @user.save
   end
+
+  def login_as(user)
+    @request.session[:user_id] = user ? user.id : nil
+  end
+
   test "should get new" do
     get :new
     assert_response :success
@@ -24,6 +29,15 @@ class SessionsControllerTest < ActionController::TestCase
     post :create, email: "invalid", password: "12"
     assert_template :new
     assert_equal "Email or password is invalid", flash[:alert]
+  end
+
+  test "should delete session when user logs out" do
+    login_as(@user)
+    delete :destroy, id: @user.id
+    assert session[:user_id].nil?
+    assert_response :redirect
+    assert_redirected_to root_url
+    assert_equal "Logged out!", flash[:notice]
   end
 
 end

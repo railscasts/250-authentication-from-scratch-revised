e6de7323b53a19bb653d408266c08fa602d0b447
Nur Sah Ketene <nursahketene@gmail.com>
2013-03-13 23:56:17 +0200

    Added test for new action

--- blog-after/test/functional/users_controller_test.rb
+++ blog-after/test/functional/users_controller_test.rb
@@ -7,5 +7,10 @@ class UsersControllerTest < ActionController::TestCase
                       password: "1234",
                       password_confirmation: "1234")
   end
+
+  test "should get new" do
+    get :new
+    assert :success
+  end
  
 end

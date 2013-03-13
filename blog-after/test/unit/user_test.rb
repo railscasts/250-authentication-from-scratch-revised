e24cae066097dfb35db0abd0939c0b634f1fe35b
Nur Sah Ketene <nursahketene@gmail.com>
2013-03-13 23:00:35 +0200

    added a test for validating password and password_confirmation presence

--- blog-after/test/unit/user_test.rb
+++ blog-after/test/unit/user_test.rb
@@ -1,18 +1,27 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "should not save the same email" do
-    user = User.new(  email: "example@email.com", 
-                      password: "1234", 
-                      password_confirmation: "1234")
-    user.save
 
+  def setup
+    @user = User.new( email: "example@email.com",
+                      password: "1234",
+                      password_confirmation: "1234"
+                      )
+  end
+  test "should not save the same email" do
+    @user.save
     invalid_user = User.new(  email: user.email, 
                               password: "1234", 
                               password_confirmation: "1234")
 
     assert invalid_user.invalid?
     assert !invalid_user.save
+  end
+
+  test "should not save without password" do
+    @user.password = ""
+    assert @user.invalid?
+    assert !@user.save
   end
   
 end

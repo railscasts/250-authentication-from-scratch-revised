2aaa783bcf72e4db36fb62b2d6b98e5c7262e792
Nur Sah Ketene <nursahketene@gmail.com>
2013-03-13 23:36:10 +0200

    Added validation for email format

--- blog-after/app/models/user.rb
+++ blog-after/app/models/user.rb
@@ -8,5 +8,11 @@ class User < ActiveRecord::Base
   ##
   # Validating presence of password on create. If you want to validate the password 
   # both on create and update remove "on: :create"
-  validates_presence_of   :password, :password_confirmation, on: :create
+  ## 
+  validates_presence_of :password, :password_confirmation, on: :create
+
+  ##
+  # Validating email format with regular expression
+  ##
+  validates_format_of   :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
 end

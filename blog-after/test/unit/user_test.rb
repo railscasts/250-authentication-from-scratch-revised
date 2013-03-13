87113f367d823a1e29d513f7b23d2b8bcd98e88b
Nur Sah Ketene <nursahketene@gmail.com>
2013-03-13 22:38:38 +0200

    Added test for email uniqueness

--- blog-after/test/unit/user_test.rb
+++ blog-after/test/unit/user_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save the same email" do
+    user = User.new(  email: "example@email.com", 
+                      password: "1234", 
+                      password_confirmation: "1234")
+    user.save
+
+    invalid_user = User.new(  email: user.email, 
+                              password: "1234", 
+                              password_confirmation: "1234")
+
+    assert invalid_user.invalid?
+    assert !invalid_user.save
+  end
+  
 end

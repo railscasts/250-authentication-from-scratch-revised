0e42fdcc90147de5a099114cdd431c3b4a2d4fb1
Nur Sah Ketene <nursahketene@gmail.com>
2013-03-13 23:01:06 +0200

    Added validation to password and password confirmation

--- blog-after/app/models/user.rb
+++ blog-after/app/models/user.rb
@@ -4,4 +4,9 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation
   
   validates_uniqueness_of :email
+
+  ##
+  # Validating presence of password on create. If you want to validate the password 
+  # both on create and update remove "on: :create"
+  validates_presence_of   :password, :password_confirmation, on: :create
 end

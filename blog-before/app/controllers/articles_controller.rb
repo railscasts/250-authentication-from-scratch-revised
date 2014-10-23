5f207be64a94c5754c114b594bf61aeb513dca36
theoblivious <hashimoto.kenn@gmail.com>
2014-10-22 20:50:26 -0400

    first commit almost done ...

--- blog-before/app/controllers/articles_controller.rb
+++ blog-before/app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_filter :authorize, only: [:edit, :update]
+
   def index
     @articles = Article.all
   end
--- blog-before/db/schema.rb
+++ blog-before/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120130184408) do
+ActiveRecord::Schema.define(:version => 20141022233142) do
 
   create_table "articles", :force => true do |t|
     t.string   "name"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(:version => 20120130184408) do
     t.datetime "published_at"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
 end
